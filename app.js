507ae65ac796e2cea7e1be41deabc901215c08a5
MiriHorvits <miri0583291861@gmail.com>
2026-07-17 03:59:01 +0300

    Initial commit - courses project

---
+++ app.js
@@ -0,0 +1,8 @@
+// סקריפט Node.js פשוט שמדפיס הודעה למסך ומבצע חישוב קטן
+console.log("שלום! הסקריפט של Node.js פועל בהצלחה!");
+
+const num1 = 5;
+const num2 = 10;
+const sum = num1 + num2;
+
+console.log(`הסכום של ${num1} ו-${num2} הוא: ${sum}`);
